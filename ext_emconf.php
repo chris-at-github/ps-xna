91872a7e8ae2f6190ea810b6957dc93bc43015f0
chris@kubuntu <pschorr.christian@gmail.com>
2018-12-21 22:51:03 +0100

    #1: Initiale Struktur von Wax

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext: "Wax"
+ ***************************************************************/
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'PS Wax',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Christian Pschorr',
+	'author_email' => 'pschorr.christian@gmail.com',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '0.1.0',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '9.5.0-9.5.99',
+			'xo' => '0.1.0-1.0.99',
+		],
+		'conflicts' => [],
+		'suggests' => [],
+	],
+	'autoload' => [
+		'psr-4' => [
+			'Ps\\Wax\\' => 'Classes',
+		],
+	],
+];
