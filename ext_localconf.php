5d908a9b4b2272fb9a9e58151a7957c2cb3c3a33
chris@kubuntu <pschorr.christian@gmail.com>
2018-12-28 10:36:02 +0100

    #5: Vererbungsstruktur für Wax Extension geschaffen

---
+++ ext_localconf.php
@@ -0,0 +1,8 @@
+<?php
+
+if(!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+
+// Konfiguration fuer den (CKE) Editor im Backend
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['xo'] = 'EXT:wax/Configuration/RTE/Default.yaml';
