b118d8c5cce4867ed80c9218755d8a3d264e69fa
Christian Pschorr <pschorr.christian@gmail.com>
2019-08-23 23:40:33 +0200

    Umbennung in XNA

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,10 @@
 <?php
 
 /***************************************************************
- * Extension Manager/Repository config file for ext: "Zn"
+ * Extension Manager/Repository config file for ext: "Xna"
  ***************************************************************/
 $EM_CONF[$_EXTKEY] = [
-	'title' => 'PS Zeen',
+	'title' => 'PS XNA',
 	'description' => '',
 	'category' => 'plugin',
 	'author' => 'Christian Pschorr',
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = [
 	],
 	'autoload' => [
 		'psr-4' => [
-			'Ps\\Zn\\' => 'Classes',
+			'Ps\\Xna\\' => 'Classes',
 		],
 	],
 ];
