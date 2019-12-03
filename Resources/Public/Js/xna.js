e6e836213a16bcd2d6881ef86cb824cd2a6c5c65
Chris <pschorr.christian@gmail.com>
2019-12-03 12:43:32 +0100

    Anpassung Darstellung Gallerien

--- Resources/Public/Js/xna.js
+++ Resources/Public/Js/xna.js
@@ -1,3 +1,11 @@
 // ---------------------------------------------------------------------------------------------------------------------
 // Elements
-require('../../../../xo/Resources/Public/Js/elements/gallery');
+const Tobi = require('../../../../xo/Resources/Public/Js/vendors/tobi');
+const Gallery = new Tobi({
+	selector: '.gallery--image',
+	captionAttribute: 'title',
+	counter: false,
+	zoom: false,
+	navLabel: ['Vorheriges Bild', 'Nächstes Bild'],
+	closeLabel: 'Bildansicht schliessen'
+});
