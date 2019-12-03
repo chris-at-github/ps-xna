1ee93fe9e1542186f319434084a59a0b4b83e5a6
Chris <pschorr.christian@gmail.com>
2019-12-03 13:42:08 +0100

    Optimierte Einbindung des Galerie Elements und Aufruf von Tobi nur wenn das Galerie Element sich auf der Seite befindet

--- Resources/Public/Js/xna.js
+++ Resources/Public/Js/xna.js
@@ -1,11 +1,19 @@
 // ---------------------------------------------------------------------------------------------------------------------
 // Elements
 const Tobi = require('../../../../xo/Resources/Public/Js/vendors/tobi');
-const Gallery = new Tobi({
-	selector: '.gallery--image',
-	captionAttribute: 'title',
-	counter: false,
-	zoom: false,
-	navLabel: ['Vorheriges Bild', 'Nächstes Bild'],
-	closeLabel: 'Bildansicht schliessen'
-});
+
+let galleries = document.getElementsByClassName('gallery');
+
+if(galleries.length !== 0) {
+	Array.prototype.forEach.call(galleries, function(gallery, index, array) {
+		let uid = gallery.getAttribute('data-gallery-uid');
+		let lightbox = new Tobi({
+			selector: '[data-gallery-uid="' + uid + '"] .gallery--image',
+			captionAttribute: 'title',
+			counter: false,
+			zoom: false,
+			navLabel: ['Vorheriges Bild', 'Nächstes Bild'],
+			closeLabel: 'Bildansicht schliessen'
+		});
+	});
+}
