8b2bf5b1e0f750ed8415d588a97555edf474693c
Chris <pschorr.christian@gmail.com>
2020-02-03 10:32:39 +0100

    Übernahme Änderungen aus isajauch.de

--- Resources/Public/Js/xna.js
+++ Resources/Public/Js/xna.js
@@ -1,3 +1,19 @@
 // ---------------------------------------------------------------------------------------------------------------------
 // Elements
-require('../../../../xo/Resources/Public/Js/elements/gallery');
+const Tobi = require('../../../../xo/Resources/Public/Js/vendors/tobi');
+
+let galleries = document.getElementsByClassName('gallery');
+
+if(galleries.length !== 0) {
+	Array.prototype.forEach.call(galleries, function(gallery, index, array) {
+		let uid = gallery.getAttribute('data-gallery-uid');
+		let lightbox = new Tobi({
+			selector: '[data-gallery-uid="' + uid + '"] .gallery--image',
+			captionAttribute: 'title',
+			counter: false,
+			zoom: false,
+			navLabel: ['Vorheriges Bild', 'Nächstes Bild'],
+			closeLabel: 'Bildansicht schliessen'
+		});
+	});
+}
