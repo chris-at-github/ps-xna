2c47697d9aaef1af7d9e757641924ebcb2cfa3e8
Christian Pschorr <c.pschorr@datamints.com>
2020-03-03 00:09:44 +0100

    Erste Version Validierung von Telefonnummern anhand eines Patterns

--- Resources/Public/Js/xna.js
+++ Resources/Public/Js/xna.js
@@ -1,19 +1,21 @@
 // ---------------------------------------------------------------------------------------------------------------------
 // Elements
-const Tobi = require('../../../../xo/Resources/Public/Js/vendors/tobi');
+// const Tobi = require('../../../../xo/Resources/Public/Js/vendors/tobi');
+//
+// let galleries = document.getElementsByClassName('gallery');
+//
+// if(galleries.length !== 0) {
+// 	Array.prototype.forEach.call(galleries, function(gallery, index, array) {
+// 		let uid = gallery.getAttribute('data-gallery-uid');
+// 		let lightbox = new Tobi({
+// 			selector: '[data-gallery-uid="' + uid + '"] .gallery--image',
+// 			captionAttribute: 'title',
+// 			counter: false,
+// 			zoom: false,
+// 			navLabel: ['Vorheriges Bild', 'Nächstes Bild'],
+// 			closeLabel: 'Bildansicht schliessen'
+// 		});
+// 	});
+// }
 
-let galleries = document.getElementsByClassName('gallery');
-
-if(galleries.length !== 0) {
-	Array.prototype.forEach.call(galleries, function(gallery, index, array) {
-		let uid = gallery.getAttribute('data-gallery-uid');
-		let lightbox = new Tobi({
-			selector: '[data-gallery-uid="' + uid + '"] .gallery--image',
-			captionAttribute: 'title',
-			counter: false,
-			zoom: false,
-			navLabel: ['Vorheriges Bild', 'Nächstes Bild'],
-			closeLabel: 'Bildansicht schliessen'
-		});
-	});
-}
+require('../../../../xo/Resources/Public/Js/components/validation/phone');
