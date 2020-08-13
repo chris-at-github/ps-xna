f7465df5017f5482b3711d1943115f1de5b71d10
Christian Pschorr <c.pschorr@datamints.com>
2020-08-13 22:21:25 +0200

    Grunddefinition von Slider Layout 0 implementiert

---
+++ Resources/Public/Js/elements/slider.js
@@ -0,0 +1,10 @@
+import { tns } from "../../../../../xo/Resources/Public/Js/vendors/tiny-slider";
+
+var slider = tns({
+	container: '.slider--container',
+	items: 1,
+	autoplay: false,
+	controls: true,
+	controlsContainer: ".slider--controls",
+	nav: false
+});
