414816bd83580b912e2e26f03870a368bb959bda
Christian Pschorr <c.pschorr@datamints.com>
2020-09-02 00:03:10 +0200

    Hx Tags in geklonten Slider Elementen entfernt

--- Resources/Public/Js/elements/slider.js
+++ Resources/Public/Js/elements/slider.js
@@ -7,16 +7,28 @@ import { tns } from '../../../../../xo/Resources/Public/Js/vendors/tiny-slider';
 
 	xna.on('documentLoaded', function() {
 		document.querySelectorAll('.slider--layout-0').forEach(function(node, index) {
+
+			let controlsContainer = node.querySelector('.slider--controls .container-inner');
+
 			let slider = tns({
-				container:         node.querySelector('.slider--container'),
-				items:             1,
-				autoplay:          false,
-				controls:          true,
-				controlsContainer: node.querySelector('.slider--controls .container-inner'),
-				nav:               true,
-				navContainer:      node.querySelector('.slider--navigation .container-inner ul'),
-				onInit:            function() {
+				container: node.querySelector('.slider--container'),
+				items: 1,
+				autoplay: false,
+				controls: true,
+				controlsContainer: controlsContainer,
+				nav: true,
+				navContainer: node.querySelector('.slider--navigation .container-inner ul'),
+				onInit: function() {
+
+					// CSS Lazyload durch setzen der Klasse slider--initialized
 					node.classList.add('slider--initialized');
+
+					// Hx-Tags in geklonten Slider Elementen entfernen
+					node.querySelectorAll('.tns-slide-cloned .slider--item-title').forEach(function(title) {
+						if(title.innerHTML.match(/<h[0-9]>/gm) !== null) {
+							title.innerHTML = title.innerText;
+						}
+					});
 				}
 			});
 		});
