831c444f4719c8cc9e1580f5f0a2db41e409cbe8
Christian Pschorr <c.pschorr@datamints.com>
2020-09-15 13:05:43 +0200

    Lightbox (Tobii) in das Modul Media Wall integriert

---
+++ Resources/Public/Js/elements/media-wall.js
@@ -0,0 +1,22 @@
+import Tobii from 'tobii/src/js/tobii';
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Layout 01
+(function () {
+	'use strict';
+
+	xna.on('documentLoaded', function() {
+		document.querySelectorAll('.media-wall--container').forEach(function(node, index) {
+			let uid = node.getAttribute('data-media-wall');
+
+			let lightbox = new Tobii({
+				selector: '[data-media-wall="' + uid + '"] .media-wall--item',
+				captionAttribute: 'title',
+				counter: false,
+				zoom: false,
+				navLabel: ['Vorheriges Element', 'Nächstes Element'],
+				closeLabel: 'Vollansicht schliessen'
+			});
+		});
+	});
+})();
