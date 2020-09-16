0ad2f3bd29231cbe8ea92a803fe291cd09a55324
Christian Pschorr <c.pschorr@datamints.com>
2020-09-16 11:21:37 +0200

    Integration Hamburger Button

--- Resources/Public/Js/components/navigation/layout-0.js
+++ Resources/Public/Js/components/navigation/layout-0.js
@@ -1,6 +1,3 @@
-// Icon Text Komponente
-// Icon Text Elemente koennen Block-Level Elemente wie Ueberschriften aufnehmen, damit diese aber trotzdem komplett
-// verlinkt werden koennen wird der Ziellink aus einem Data-Attribute ausgewertet
 (function () {
 	'use strict';
 
