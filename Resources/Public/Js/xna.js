cccb84d0853d2d86e9db544660edac2978600b05
Christian Pschorr <c.pschorr@datamints.com>
2020-09-29 22:48:25 +0200

    Google Maps Modul aus JS Datei entfernt

--- Resources/Public/Js/xna.js
+++ Resources/Public/Js/xna.js
@@ -1,27 +1,4 @@
 // ---------------------------------------------------------------------------------------------------------------------
-// Elements
-// const Tobi = require('../../../../xo/Resources/Public/Js/vendors/tobi');
-//
-// let galleries = document.getElementsByClassName('gallery');
-//
-// if(galleries.length !== 0) {
-// 	Array.prototype.forEach.call(galleries, function(gallery, index, array) {
-// 		let uid = gallery.getAttribute('data-gallery-uid');
-// 		let lightbox = new Tobi({
-// 			selector: '[data-gallery-uid="' + uid + '"] .gallery--image',
-// 			captionAttribute: 'title',
-// 			counter: false,
-// 			zoom: false,
-// 			navLabel: ['Vorheriges Bild', 'Nächstes Bild'],
-// 			closeLabel: 'Bildansicht schliessen'
-// 		});
-// 	});
-// }
-
-//require('../../../../xo/Resources/Public/Js/components/validation/phone');
-
-
-// ---------------------------------------------------------------------------------------------------------------------
 // Helper | Polyfills
 require('./polyfills/foreach');
 require('./polyfills/object-assign');
@@ -39,6 +16,5 @@ require('./components/navigation/layout-0');
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Elements
-require('./elements/google-map');
 require('./elements/slider');
 require('./elements/media-wall');
