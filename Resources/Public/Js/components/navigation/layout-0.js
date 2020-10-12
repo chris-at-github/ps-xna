11bdf3bb1cbcf9c2521c37f462a117ed39662ebf
Christian Pschorr <c.pschorr@datamints.com>
2020-10-12 21:56:12 +0200

    Ein- und Ausblendlogik der mobilen Navigation implementiert

--- Resources/Public/Js/components/navigation/layout-0.js
+++ Resources/Public/Js/components/navigation/layout-0.js
@@ -4,9 +4,33 @@
 	xna.on('documentLoaded', function() {
 		let body = document.querySelector('body');
 
+		document.querySelectorAll('#header .navigation a').forEach(function(node) {
+			node.addEventListener('click', function(event) {
+				if(document.body.classList.contains('is--sidebar-active') === true) {
+					document.body.classList.add('is--sidebar-closing');
+					document.body.classList.remove('is--sidebar-active');
+
+					document.querySelector('#header .sidebar').addEventListener('transitionend', function() {
+						document.body.classList.remove('is--sidebar-closing');
+					});
+
+					// Scrollbars ausblenden
+					xna.fireEvent('scrolllock.toggle');
+					event.preventDefault();
+				}
+			});
+		});
+
 		document.querySelectorAll('#header .hamburger').forEach(function(node) {
 			node.addEventListener('click', function() {
-				body.classList.toggle('is--sidebar-active');
+
+				if(document.body.classList.contains('is--sidebar-active') === true) {
+					document.body.classList.remove('is--sidebar-active');
+					// xna.fireEvent('scrolllock.deactivate');
+
+				} else {
+					document.body.classList.add('is--sidebar-active');
+				}
 
 				// Scrollbars ausblenden
 				xna.fireEvent('scrolllock.toggle');
