3c34eb4f3a5889df348cb42a4eb6ba8998a66058
Christian Pschorr <c.pschorr@datamints.com>
2020-12-30 17:20:19 +0100

    Basiseinrichtung von Modals für die Detailansicht eines Radels

--- Resources/Public/Js/elements/bicycle-market.js
+++ Resources/Public/Js/elements/bicycle-market.js
@@ -1,12 +1,20 @@
-import { tns } from "tiny-slider/src/tiny-slider"
+import { tns } from 'tiny-slider/src/tiny-slider';
+import modal from 'micromodal';
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Bicycle Detail View
 (function () {
 	'use strict';
 
-	xna.on('bicycleLoaded', function() {
+	xna.on('bicycleOpen', function() {
+		modal.show('modal-1', {
+			onShow: modal => console.info(`${modal.id} is shown`), // [1]
+			onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+			openClass: 'is--modal-open' // [5]
+		});
+	});
 
+	xna.on('bicycleLoaded', function() {
 		document.querySelectorAll('.slider').forEach(function(node, index) {
 			let slider = tns({
 				container: node.querySelector('.slider--container'),
