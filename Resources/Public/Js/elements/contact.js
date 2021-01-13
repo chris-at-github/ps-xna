e817e9c380884e443f58af3338e41a619649f4dc
Christian Pschorr <c.pschorr@datamints.com>
2021-01-13 22:39:08 +0100

    Absicherungslogik für das Abschicken des Formulars erstellt -> kein versehentliches Abschicken mit fehlerhaften Daten

--- Resources/Public/Js/elements/contact.js
+++ Resources/Public/Js/elements/contact.js
@@ -20,6 +20,7 @@
 		document.querySelectorAll('.ce-contact-search').forEach(function(node, index) {
 			let form = node.querySelector('form');
 			let resultContainer = node.querySelector('.contact-search--result-container');
+			let zipRegex = '';
 
 			form.addEventListener('submit', function(event) {
 				let data = new FormData(form);
@@ -42,6 +43,54 @@
 					});
 
 				event.preventDefault();
+			});
+
+			// Laenderauswahl
+			node.querySelector('.contact-search--countries').addEventListener('change', function(event) {
+				let value = parseInt(this.value);
+
+				let target = {
+					zip: node.querySelector('.contact-search--zip'),
+					button: node.querySelector('button[type="submit"]')
+				};
+
+				// kein Land ausgewaehlt -> Versand des Formulars verhindern
+				if(value === 0) {
+
+					// Button sperren
+					target.button.disabled = true;
+
+				} else if(typeof(xna.data.contact[value]) !== 'undefined') {
+
+					// Button freigeben
+					target.button.disabled = false;
+
+					// es wurde ein Eintrag ausgewaehlt zu dem es eine Konfiguration gibt -> also keine PLZ-Eingabe noetig
+					// 1. Zip Feld freischalten
+					// 2. Ober-Variable zipRegex beschreiben
+					if(xna.data.contact[value].zipRegex !== '') {
+						target.zip.disabled = false;
+						zipRegex = xna.data.contact[value].zipRegex;
+
+					// 1. Zip Feld sperren
+					// 2. Zip Feld leeren
+					} else {
+						target.zip.value = '';
+						target.zip.disabled = true;
+					}
+				}
+			});
+
+			// Zip Eingabe
+			node.querySelector('.contact-search--zip').addEventListener('keypress', function(event) {
+				if(zipRegex !== '') {
+					let regex = new RegExp(zipRegex, 'g');
+					let value = this.value + String.fromCharCode(event.charCode || event.keyCode);
+
+					if(value.match(regex) === null) {
+						event.preventDefault();
+					}
+				}
 			});
 		});
 	});
