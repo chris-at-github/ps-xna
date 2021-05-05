e5d633647cf2f2eba0cffda370f5d92c16752b3f
Christian Pschorr <c.pschorr@datamints.com>
2021-05-05 23:23:01 +0200

    Grunddefinition und Installation des Moduls Gallery erstellt

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,6 +22,7 @@ $EM_CONF[$_EXTKEY] = [
 			'ce_icon_text' => '1.0.0-1.99.99',
 			'ce_hero' => '1.0.0-1.99.99',
 			'ce_facts' => '1.0.0-1.99.99',
+			'ce_gallery' => '1.0.0-1.99.99',
 		],
 		'conflicts' => [],
 		'suggests' => [],
