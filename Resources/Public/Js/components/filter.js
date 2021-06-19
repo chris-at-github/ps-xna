28ab780ef040d2f2630e35b080911fe9521a4ec1
Christian Pschorr <c.pschorr@datamints.com>
2021-06-19 12:07:26 +0200

    Filter auf Produktauflistungen limitiert

--- Resources/Public/Js/components/filter.js
+++ Resources/Public/Js/components/filter.js
@@ -4,22 +4,13 @@ import Filter from '../modules/filter';
 	'use strict';
 
 	xna.on('documentLoaded', function() {
-		let filter = new Filter(document.querySelector('.product--listing'), {
-			ajax: true,
-			pageType: 1548191072,
-			containerSelector: '.card--container',
-			itemsSelector: '.card'
-		});
-
-		// let body = document.querySelector('body');
-		//
-		// document.querySelectorAll('#header .hamburger').forEach(function(node) {
-		// 	node.addEventListener('click', function() {
-		// 		body.classList.toggle('is--off-canvas-active');
-		//
-		// 		// Scrollbars ausblenden
-		// 		xna.fireEvent('scrolllock.toggle');
-		// 	}, false);
-		// });
+		if(document.querySelector('.product--listing') !== null) {
+			let filter = new Filter(document.querySelector('.product--listing'), {
+				ajax: true,
+				pageType: 1548191072,
+				containerSelector: '.card--container',
+				itemsSelector: '.card'
+			});
+		}
 	});
 })();
