34a3cb0c6e4a99603b07f65a99cc222f5f73ab49
Christian Pschorr <c.pschorr@datamints.com>
2021-10-17 21:45:09 +0200

    Basisstyling der (Produkt-) Filter

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,3 +7,6 @@ if(!defined('TYPO3_MODE')) {
 // Konfiguration fuer den (CKE) Editor im Backend
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['xoDefault'] = 'EXT:xna/Configuration/RTE/Default.yaml';
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['xoMinimal'] = 'EXT:xna/Configuration/RTE/Minimal.yaml';
+
+// Language Override
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:xo/Resources/Private/Language/locallang_frontend.xlf'][] = 'EXT:xna/Resources/Private/Language/locallang_frontend.xlf';
