9a5da129583da00dff29746e5908924d2e9b9138
Christian Pschorr <c.pschorr@datamints.com>
2022-01-05 10:41:15 +0100

    Indexierung von Sub-Sub-Inhalten vorbereitet -> Inhalte von der PID werden sowieso bereits ausgelesen

--- Classes/KeSearchIndexer/ContentElementRecords.php
+++ Classes/KeSearchIndexer/ContentElementRecords.php
@@ -2,6 +2,7 @@
 
 namespace Ps\Xna\KeSearchIndexer;
 
+use Tpwd\KeSearch\Indexer\Types\Page;
 use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
@@ -22,10 +23,10 @@ class ContentElementRecords {
 	/**
 	 * @param string $bodytext
 	 * @param array $ttContent
-	 * @param $pageIndexer
+	 * @param Page $pageIndexer
 	 * @return void
 	 */
-	public function modifyContentFromContentElement(string &$bodytext, array $ttContent, $pageIndexer) {
+	public function modifyContentFromContentElement(string &$bodytext, array $ttContent, Page $pageIndexer) {
 		if(empty($ttContent['tx_xo_elements']) === true) {
 			return;
 		}
@@ -33,7 +34,7 @@ class ContentElementRecords {
 		/** @var QueryBuilder $queryBuilder */
 		$queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('tx_xo_domain_model_elements')->createQueryBuilder();
 		$statement = $queryBuilder
-			->select('uid', 'title', 'description')
+			->select('uid', 'title', 'description', 'content')
 			->from('tx_xo_domain_model_elements')
 			->where(
 				$queryBuilder->expr()->eq('foreign_uid', $queryBuilder->createNamedParameter((int) $ttContent['uid'], Connection::PARAM_INT)),
@@ -51,4 +52,26 @@ class ContentElementRecords {
 			}
 		}
 	}
+
+//	protected function indexChildContentElements($parentUid, $sysLanguageUid, Page $pageIndexer) {
+//		DebuggerUtility::var_dump($parentUid, $sysLanguageUid);
+//		$x = '';
+//
+//		/** @var QueryBuilder $queryBuilder */
+//		$queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('tt_content')->createQueryBuilder();
+//		$statement = $queryBuilder
+//			->select('*')
+//			->from('tt_content')
+//			->where(
+//				$queryBuilder->expr()->eq('tx_xo_parent', $queryBuilder->createNamedParameter($parentUid, Connection::PARAM_INT)),
+//				$queryBuilder->expr()->eq('sys_language_uid', $queryBuilder->createNamedParameter($sysLanguageUid, Connection::PARAM_INT))
+//			)
+//			->execute();
+//
+//		while($row = $statement->fetch()) {
+//			$x .= $pageIndexer->getContentFromContentElement($row);
+//		}
+//
+//		DebuggerUtility::var_dump($x);
+//	}
 }
