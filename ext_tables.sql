12e2a9f22afb25905c4db1dfcfe94433bb35b435
Christian Pschorr <pschorr.christian@gmail.com>
2022-08-30 23:01:45 +0200

    Feld Additional Description in XO Extension übertragen und ausgegeben

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,7 +5,3 @@ CREATE TABLE tt_content (
 	tx_xna_wkhtmltopdf_enabled smallint(5) unsigned DEFAULT '0' NOT NULL,
 	tx_xna_wkhtmltopdf_title varchar(255) NOT NULL DEFAULT ''
 );
-
-CREATE TABLE tt_address (
-	tx_xna_additional_description text
-);
