a6678f702a9a9bce6a8138b91f123e3a9f470e11
Christian Pschorr <pschorr.christian@gmail.com>
2022-11-17 23:29:58 +0100

    Umbrüche in Item Überschriften ermöglichen

---
+++ Configuration/TCA/Overrides/tx_xo_domain_model_elements.php
@@ -0,0 +1,7 @@
+<?php
+
+$GLOBALS['TCA']['tx_xo_domain_model_elements']['types']['icon_text_default']['columnsOverrides']['title']['config'] = [
+	'type' => 'text',
+	'rows' => 2,
+	'cols' => 30,
+];
