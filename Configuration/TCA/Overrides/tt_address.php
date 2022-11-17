2a71b84781bd1e5fc3d98ff95f58c9ee5865eb6e
Christian Pschorr <pschorr.christian@gmail.com>
2022-11-17 22:52:08 +0100

    Fix Sortierung von Ansprechpartnern -> Entfernung von Datensätzen ohne Kontinent- und ohne Länderzuordnung

--- Configuration/TCA/Overrides/tt_address.php
+++ Configuration/TCA/Overrides/tt_address.php
@@ -1,4 +1,4 @@
 <?php
 
-$GLOBALS['TCA']['tt_address']['ctrl']['label_alt'] = 'name, last_name, first_name, company';
+$GLOBALS['TCA']['tt_address']['ctrl']['label_alt'] = 'last_name, first_name, company';
 $GLOBALS['TCA']['tt_address']['ctrl']['label_alt_force'] = true;
