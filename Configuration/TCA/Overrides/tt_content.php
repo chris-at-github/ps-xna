0f6bccc78770b902908dfb648561144d821e5d1b
Christian Pschorr <pschorr.christian@gmail.com>
2023-07-01 00:38:23 +0200

    Neues Content Element für Grundrisse erstellt

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -61,3 +61,58 @@ new \B13\Container\Tca\ContainerConfiguration(
 ));
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', '--palette--;;xoBackgroundMedia', 'ps14_xna_furnishing', 'after:space_after_class');
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Special Sketch Module
+// ---------------------------------------------------------------------------------------------------------------------
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+	array(
+		'LLL:EXT:xna/Resources/Private/Language/locallang_tca.xlf:sketch.title',
+		'ps14_xna_sketch',
+		'content-image'
+	),
+	'CType',
+	'xna'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['ps14_xna_sketch'] = [
+	'showitem' => '
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.general;general,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.header;xoHeader, tx_xo_elements,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.appearance,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.frames;frames,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
+			--palette--;;hidden,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.visibility;visibility,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.access;access,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.extended
+	',
+];
+
+//$GLOBALS['TCA']['tt_content']['types']['ps14_hero_slider']['columnsOverrides']['tx_xo_elements']['config']['foreign_label'] = 'uid';
+$GLOBALS['TCA']['tt_content']['types']['ps14_xna_sketch']['columnsOverrides']['tx_xo_elements']['config']['overrideChildTca'] = [
+	'columns' => [
+		'record_type' => [
+			'config' => [
+				'items' => [
+					['LLL:EXT:xna/Resources/Private/Language/locallang_tca.xlf:tx_xo_domain_model_elements.record_type.sketch_floor', 'ps14_xna_sketch_floor'],
+				],
+				'default' => 'ps14_xna_sketch_floor'
+			]
+		],
+		'description' => [
+			'config' => [
+				'richtextConfiguration' => 'xoDefault'
+			]
+		]
+	],
+	'types' => [
+		'ps14_xna_sketch_floor' => [
+			'showitem' => '
+				l10n_diffsource, record_type, title, description, media,
+				--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
+				--palette--;;visibility,
+				--palette--;;access',
+		],
+	]
+];
